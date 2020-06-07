ed9f8688724199bc11c8b13029a2f251ebcfd65a
Fly Chang <leo424y@gmail.com>
2020-06-07 18:20:22 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/c-u.be",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `c-u.be`,
+    description: `c-u.be`,
+    author: `@leo424y`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
